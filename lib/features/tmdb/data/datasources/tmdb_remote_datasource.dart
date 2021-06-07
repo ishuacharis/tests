75051af2a1fdcc14cc6f456f2f9d831bf571c680
ishuacharis <ishuacharis@gmail.com>
2021-06-07 15:19:01 +0100

    Implement getSearchMovie and getSearchArtist in tmdb_remote_datasource.dart File

--- lib/features/tmdb/data/datasources/tmdb_remote_datasource.dart
+++ lib/features/tmdb/data/datasources/tmdb_remote_datasource.dart
@@ -43,6 +43,19 @@ class TmdbRemoteDataSourceImpl implements TmdbRemoteDataSource {
     return MovieModel.fromJson(movie);
   }
 
+  @override
+  Future<MovieModel> getSearchMovie(String searchText) async {
+    final movie = await _getSearchMovie(searchText);
+    return MovieModel.fromJson(movie);
+  }
+
+
+  @override
+  Future<PersonModel> getSearchPerson(String searchText) async {
+    final artist  = await _getSearchPerson(searchText);
+    return PersonModel.fromJson(artist);
+  }
+
 
   dynamic _getAllPeople() async {
     try{
@@ -135,6 +148,68 @@ class TmdbRemoteDataSourceImpl implements TmdbRemoteDataSource {
 
 
   }
+
+  dynamic _getSearchMovie(String searchText) async {
+    var endPoint = "https://api.themoviedb.org/3/movie/$searchText?api_key=$API_KEY";
+    try{
+      var parseUri = Uri.parse(endPoint);
+
+      http.Response response =  await client.get(parseUri);
+
+      Map<String,dynamic> results = json.decode(response.body);
+      if(response.statusCode != 200) {
+        throw Exception("Unable to process url");
+      }
+      return results;
+
+    } on SocketException {
+
+      throw NetworkException(error: "Please check your internet settings");
+    } on HttpException {
+
+      throw ServerException(error: "Internal server error");
+    } on FormatException {
+
+      throw InvalidFormatException(error: "Please check your data");
+    }
+    catch(e) {
+
+      throw Exception(e.toString());
+    }
+
+
+  }
+
+  dynamic _getSearchPerson(String searchText) async {
+    var endPoint = "https://api.themoviedb.org/3/person/$searchText?api_key=$API_KEY";
+    try{
+      var parseUri = Uri.parse(endPoint);
+
+      http.Response response =  await client.get(parseUri);
+
+      Map<String,dynamic> results = json.decode(response.body);
+      if(response.statusCode != 200) {
+        throw Exception("Unable to process url");
+      }
+      return results;
+
+    } on SocketException {
+
+      throw NetworkException(error: "Please check your internet settings");
+    } on HttpException {
+
+      throw ServerException(error: "Internal server error");
+    } on FormatException {
+
+      throw InvalidFormatException(error: "Please check your data");
+    }
+    catch(e) {
+
+      throw Exception(e.toString());
+    }
+  }
+
+
 
 
 }
